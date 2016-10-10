f57314ac1ecc1fe4a768caff7b06a45a67878bc1
johnm0nty <montyoverzero@gmail.com>
2016-10-09 19:07:15 -0700

    default data for room table

--- script.sql
+++ script.sql
@@ -3,6 +3,7 @@ create procedure setupdb()
 begin
   if not exists (select * from information_schema.tables where table_name = 'rooms') then
     create table ROOMS (ID int primary key, NAME varchar(128) unique not null, DATE_CREATED datetime default current_timestamp, DATE_MODIFIED timestamp);
+    insert into rooms (id,name) values (1,'Sim City'),(2,'Zelda'),(3,'Sonic'),(4,'Halo');
 	end if;
 end//
 delimiter ;
