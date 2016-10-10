90090aa7c684ce6adf39071ea043966a55fe5917
johnm0nty <montyoverzero@gmail.com>
2016-10-09 17:34:34 -0700

    added room table to script

--- script.sql
+++ script.sql
@@ -1,8 +1,8 @@
 delimiter //
 create procedure setupdb()
 begin
-  if not exists(select 1) then select 2;
-    else select 3;
+  if not exists (select * from information_schema.tables where table_name = 'rooms') then
+    create table ROOMS (ID int primary key, NAME varchar(128) unique not null, DATE_CREATED datetime default current_timestamp, DATE_MODIFIED timestamp);
 	end if;
 end//
 delimiter ;
